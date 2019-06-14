027ce57037d4f994846da0d323c693c4b9503547
Cytzen <caldeireiro.arthur@gmail.com>
2019-06-14 11:07:38 +0200

    Adjustment

--- Automatique/Automatique.ino
+++ Automatique/Automatique.ino
@@ -26,25 +26,26 @@ void loop() {
 
   //Tout les capteurs Blancs
   if ( digitalRead(4) != HIGH && digitalRead(3) != HIGH && digitalRead(2) != HIGH && state < 200) {
-    setSpeed (60, 60);
+    setSpeed (65, 65);    // AVANCE PENDANT 200 TOUR DE LOOP
     state = state + 1;
   }
   if ( digitalRead(4) != HIGH && digitalRead(3) != HIGH && digitalRead(2) != HIGH && state >= 200) {
-    setSpeed (0, 0);
+    setSpeed (0, 0);     // STOPE
   }
-  
   //Tout les capteurs Noirs
   if ( digitalRead(4) == HIGH && digitalRead(3) == HIGH && digitalRead(2) == HIGH ) {
-    setSpeed (80, -70);   // TOURNE DROITE
-    delay (800);
-    state = 0;
+    setSpeed (65, 65);   // TOURNE DROITE
+    delay (300);
+    setSpeed (80, -70);
+    delay (600);
+    state =0;
   }
 
 
 
   // Capteur milieu Noir
   if (  digitalRead(4) != HIGH && digitalRead(3) == HIGH && digitalRead(2) != HIGH ) {
-    setSpeed (70, 70);    // AVANCE
+    setSpeed (65, 65);    // AVANCE
     state = 0;
   }
   // Capteur gauche Noir
@@ -61,21 +62,27 @@ void loop() {
 
   // Capteurs milieu et gauche Noirs
   if (  digitalRead(4) == HIGH && digitalRead(3) == HIGH && digitalRead(2) != HIGH ) {
-    setSpeed (-70, 80);   // TOURNE GAUCHE
-    delay (800);
-    state = 0;
+    setSpeed (65, 65);   // TOURNE GAUCHE
+    delay (300);
+    setSpeed (-80, 80);
+    delay (600);
+    state =0;
   }
   // Capteurs milieu et droit Noirs
   if (  digitalRead(4) != HIGH && digitalRead(3) == HIGH && digitalRead(2) == HIGH ) {
-    setSpeed (80, -70);   // TOURNE DROITE
-    delay (800);
-    state = 0;
+    setSpeed (65, 65);   // TOURNE DROITE
+    delay (300);
+    setSpeed (80, -80);
+    delay (600);
+    state =0;
   }
   // Capteur gauche et droit Noirs
   if (  digitalRead(4) == HIGH && digitalRead(3) != HIGH && digitalRead(2) == HIGH ) {
-    setSpeed (80, -70);   // TOURNE DROITE
-    delay (800);
-    state = 0;
+    setSpeed (65, 65);   // TOURNE DROITE
+    delay (300);
+    setSpeed (80, -80);
+    delay (600);
+    state =0;
   }
 
 
@@ -87,5 +94,4 @@ void loop() {
 void setSpeed (int GSpeed, int DSpeed) {
   Motor.speed(MOTOR1, GSpeed);  // Set speed of MOTOR1
   Motor.speed(MOTOR2, DSpeed);  // Set speed of MOTOR2
-
 }
